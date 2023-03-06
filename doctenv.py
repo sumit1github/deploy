f2b055d0a1b95595605fba3aeecc60dabd5e71d3
Sumit <102658947+sumit1github@users.noreply.github.com>
2023-03-06 13:26:04 +0530

    Create doctenv.py

---
+++ doctenv.py
@@ -0,0 +1,16 @@
+pip install python-dotenv
+
+create  .env file in root directory and add .env in git ignore
+
+# create secure secret key
+python manage.py shell
+from django.core.management.utils import get_random_secret_key
+print(get_random_secret_key())
+
+
+--------------------------------------------settings.py-----------------------------------------
+
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
